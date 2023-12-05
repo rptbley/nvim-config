73dce5538df995ffa97b6a637e6f18e00c2fd747
방우현 <docter4@naver.com>
2023-12-05 21:08:32 +0900

    change darcula plugin

--- lua/plugins/darcula.lua
+++ lua/plugins/darcula.lua
@@ -1,10 +1,7 @@
 local M = {
-  'briones-gabriel/darcula-solid.nvim',
-  dependencies = {
-    'rktjmp/lush.nvim'
-  },
+  'doums/darcula',
   init = function()
-    vim.cmd 'colorscheme darcula-solid'
+    vim.cmd 'colorscheme darcula'
     vim.cmd 'set termguicolors'
   end
 }
