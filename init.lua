63d0442cc172bb72d3b69343c9cd18c08970d528
방우현 <docter4@naver.com>
2023-12-08 08:39:20 +0900

    add jdtls

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 require("config/settings")
 require("config/lazy")
 require("config/keymaps")
+
+vim.cmd [[
+augroup jdtls_lsp
+    autocmd!
+    autocmd FileType java lua require'jdtls.jdtls_setup'.setup()
+augroup end
+]]
