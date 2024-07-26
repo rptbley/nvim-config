5545687228fff915bdf51a64972cac7bbbfdb830
방우현 <docter4@naver.com>
2024-07-26 15:12:48 +0900

    add filepath autocompletion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -43,6 +43,7 @@ local cmp = {
         end, { "i", "s" }),
       }),
       sources = cmp.config.sources({
+        { name = 'path' },
         { name = "nvim_lsp_signature_help" },
         { name = "nvim_lsp" },
         { name = "luasnip" },
