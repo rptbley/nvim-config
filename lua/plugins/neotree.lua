f33a06ae1e8de96156589a0f9426ccee97ec91cb
방우현 <docter4@naver.com>
2024-07-31 15:40:39 +0900

    fix neotree icon issue, add select ui plugin

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,20 +1,49 @@
 local M = {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v2.x",
-	dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
-	config = function()
-		require("neo-tree").setup({
-			source_selector = {
-				winbar = true,
-			},
-			window = {
-				mappings = {
-					["o"] = "open",
-					["<Tab>"] = "next_source",
-				},
-			},
-		})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v2.x",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+  config = function()
+    require("neo-tree").setup({
+      source_selector = {
+        winbar = true,
+        sources = {
+          {
+            source = "filesystem",
+            display_name = " 󰉓 Files "
+          },
+          {
+            source = "buffers",
+            display_name = " 󰈚 Buffers "
+          },
+          {
+            source = "git_status",
+            display_name = " 󰊢 Git "
+          },
+        },
+      },
+      window = {
+        mappings = {
+          ["o"] = "open",
+          ["<Tab>"] = "next_source",
+        },
+      },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            added     = "✚",
+            modified  = "",
+            deleted   = "✖",
+            renamed   = "󰁕",
+            untracked = "",
+            ignored   = "",
+            unstaged  = "󰄱",
+            staged    = "",
+            conflict  = "",
+          }
+        },
+      }
+    })
+  end,
 }
 
 return { M }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,38 @@
 local M = {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope-ui-select.nvim' },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
 
-		telescope.setup({
-			defaults = {
-				initial_mode = "normal",
-				mappings = {
-					i = {
-						["<C-h>"] = "which_key",
-					},
-					n = {
-						["q"] = actions.close,
-						["o"] = actions.select_default,
-					},
-				},
-			},
-			pickers = {
-				find_files = { theme = "dropdown" },
-				live_grep = { theme = "dropdown" },
-				buffers = { theme = "dropdown" },
-				oldfiles = { theme = "dropdown" },
-			},
-		})
-	end,
+    telescope.setup({
+      defaults = {
+        initial_mode = "normal",
+        mappings = {
+          i = {
+            ["<C-h>"] = "which_key",
+          },
+          n = {
+            ["q"] = actions.close,
+            ["o"] = actions.select_default,
+          },
+        },
+      },
+      pickers = {
+        find_files = { theme = "dropdown" },
+        live_grep = { theme = "dropdown" },
+        buffers = { theme = "dropdown" },
+        oldfiles = { theme = "dropdown" },
+      },
+      extensions = {
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown()
+        }
+      }
+    })
+
+    telescope.load_extension('ui-select')
+  end,
 }
 
 return { M }
