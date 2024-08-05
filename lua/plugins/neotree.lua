dba87fdfbf5429837af2c39316cf7be44db13fd0
방우현 <docter4@naver.com>
2024-08-05 17:34:15 +0900

    remove image.nvim

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,7 +1,7 @@
 local M = {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", "3rd/image.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
   config = function()
     require("neo-tree").setup({
       window = {
